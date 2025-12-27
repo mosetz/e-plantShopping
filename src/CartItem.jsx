d6b3e9731f0fb45e139aa37521bf9c73d853a914
Sarit Samkumpim <saritsamkumpim@gmail.com>
2025-12-27 20:17:14 +0700

    adjust identation

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -60,15 +60,15 @@ const CartItem = ({ onContinueShopping }) => {
             <div className="cart-item" key={item.name}>
                 <img className="cart-item-image" src={item.image} alt={item.name} />
                 <div className="cart-item-details">
-                <div className="cart-item-name">{item.name}</div>
-                <div className="cart-item-cost">{item.cost}</div>
-                <div className="cart-item-quantity">
-                    <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
-                    <span className="cart-item-quantity-value">{item.quantity}</span>
-                    <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
-                </div>
-                <div className="cart-item-total">Total: ${calculateTotalCost(item)}</div>
-                <button className="cart-item-delete" onClick={() => handleRemove(item)}>Delete</button>
+                    <div className="cart-item-name">{item.name}</div>
+                    <div className="cart-item-cost">{item.cost}</div>
+                    <div className="cart-item-quantity">
+                        <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
+                        <span className="cart-item-quantity-value">{item.quantity}</span>
+                        <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
+                    </div>
+                    <div className="cart-item-total">Total: ${calculateTotalCost(item)}</div>
+                    <button className="cart-item-delete" onClick={() => handleRemove(item)}>Delete</button>
                 </div>
             </div>
             ))}
